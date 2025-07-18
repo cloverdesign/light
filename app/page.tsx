2fb8b4b31f7cc0b392e2eb7d69bfc8595f9fa512
tanthehack <31630356+tanthehack@users.noreply.github.com>
2025-07-18 10:25:25 +0100

    fix: hero image file extension

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ import hero1 from "@/assets/images/hero1.png";
 import hero2 from "@/assets/images/hero2.png";
 import hero3 from "@/assets/images/hero3.png";
 import hero4 from "@/assets/images/hero4.png";
-import hero5 from "@/assets/images/hero5.png";
+import hero5 from "@/assets/images/hero5.webp";
 import circle from "@/assets/images/circle.png";
 import circle2 from "@/assets/images/circle2.png";
 import Link from "next/link";
